73c34be5cc51fd9b743242773decd09024f960ea
nvmdang2509 <mrsoc2509@gmail.com>
2020-11-18 21:38:48 +0700

    update 18/11/20

---
+++ playgrounds/index.js
@@ -0,0 +1,7 @@
+// 1.. Let , const
+// phạm vi sử dụng  let va const chi duoc dung trong 1 cap ngoac {}
+// let và const không được khai báo 2 biến giống nhau cùng 1 khu vực 
+// js có 2 kiểu dữ liệu THAM TRỊ và THAM CHIÉU 
+var num = 1 ;
+let num1 = 2;
+const pi = 3.15;
